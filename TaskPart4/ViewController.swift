9ef9954b3f6b9438e509cc16003029a8eebea5de
kapisan <60970645 + kapisan @ users.noreply.github.com>
2021-02-11 20:48:05 +0900

    アプリのコード完成

--- TaskPart4/ViewController.swift
+++ TaskPart4/ViewController.swift
@@ -8,16 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var countButton: UIButton!
-    @IBOutlet weak var clear: UIButton!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    var countValue = 0
+
+    @IBAction func countBtnAction(_ sender: UIButton) {
+        countValue += 1
+        label.text = String(countValue)
     }
 
-
+    @IBAction func clearBtnAction(_ sender: UIButton) {
+        countValue = 0
+        label.text = String(countValue)
+    }
 }
-
